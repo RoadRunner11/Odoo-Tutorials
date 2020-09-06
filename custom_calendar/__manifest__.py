efbf1c7d12a83aae07e6b487a3eae60bd55e5cf9
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-09-06 11:10:22 +0100

    commit chnages

--- custom_calendar/__manifest__.py
+++ custom_calendar/__manifest__.py
@@ -1,22 +1,28 @@
+
 # -*- coding: utf-8 -*-
 {
-    'name': "custom_calendar",
+    'name': "tutorials",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       An application to keep records of all tutorials
+    		""",
 
     'description': """
-        Long description of module's purpose
+        The Tutorials application will help us keep proper
+        records of our works. This means we can store our
+        tasks and the tutorials attached to these tasks in this 
+        Tutorials application. By doing this, we can make sure 
+        that we are losing out any important 
+        information relating to works, both internal and external works.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "AKOREDE FODILU OLAWALE",
+    'website': "http://46.101.249.182:8069",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tutorials',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +30,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
--- custom_calendar/models/models.py
+++ custom_calendar/models/models.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class custom_calendar(models.Model):
-#     _name = 'custom_calendar.custom_calendar'
-#     _description = 'custom_calendar.custom_calendar'
+class custom_calendar(models.Model):
+    _name = 'custom_calendar.custom_calendar'
+    _description = 'custom_calendar.custom_calendar'
 
-#     name = fields.Char()
+    name = fields.Char(string='Event Name')
+    start = fields.Datetime()
+    end = fields.Datetime()
+    duration = fields.Float( compute='_compute_duration', size=16, digits=(16, 0), required=True)
+
+    @api.depends('start', 'end')	
+    def _compute_duration(self):
+        for record in self:
+            record.duration = record.end -record.start
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
