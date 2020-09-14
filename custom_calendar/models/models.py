ad23329f1607206e5e87ae4abbaa17567f071475
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-09-14 01:57:20 +0100

    commit

--- custom_calendar/models/models.py
+++ custom_calendar/models/models.py
@@ -8,15 +8,15 @@ class custom_calendar(models.Model):
     _description = 'custom_calendar.custom_calendar'
 
     name = fields.Char(string='Event Name')
-    start = fields.Date(string="Start date")
-    end = fields.Date(string="End date")
-    duration = fields.Float(  string="TOTAL DAYS")
+    start = fields.Datetime(string="Start date")
+    end = fields.Datetime(string="End date")
+    duration = fields.Integer(  string=" Duration")
 
     @api.onchange('start', 'end', 'duration')
     def calculate_date(self):
         if self.start and self.end:
-            d1=datetime.strptime(str(self.start),'%Y-%m-%d') 
-            d2=datetime.strptime(str(self.end),'%Y-%m-%d')
+            d1=datetime.strptime(str(self.start),'%Y-%m-%d %H:%M:%S') 
+            d2=datetime.strptime(str(self.end),'%Y-%m-%d %H:%M:%S')
             d3=d2-d1
             self.duration=str(d3.days)
     # @api.depends('start', 'end')	
