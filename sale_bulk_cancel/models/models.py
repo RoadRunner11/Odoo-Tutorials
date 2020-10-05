91e7dc880ff8a246c7c95c8c706f8380cebc2da0
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-10-05 14:40:23 +0100

    add

--- sale_bulk_cancel/models/models.py
+++ sale_bulk_cancel/models/models.py
@@ -3,20 +3,18 @@
 from odoo import models, fields, api
 
 
-class sale_bulk_cancel(models.TransientModel):
-    _name = "bulk.cancel"
-    _description = 'This allows a user to cancel all checked orders'
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-    def action_cancel(self):
-        active_ids = self._context.get("active_ids", []) or []
-        for record in self.env["sale.order"].browse(active_ids):
-            record.state = "Cancelled"
-
+class ChangeState(models.TransientModel):
+    _name = 'update.state'
+    _description = 'Change the state of sale order'
+    
+    state = fields.Selection([
+        ('draft', 'Quotation'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled'),
+    ], string = 'Status')
+    def update_state(self):
+        active_ids = self._context.get('active_ids', []) or []
+        for record in self.env['sale.order'].browse(active_ids):
+            record.state = self.state
