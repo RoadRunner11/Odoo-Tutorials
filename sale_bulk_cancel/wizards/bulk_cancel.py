9855b84bf1c578811696535df6a1c1e7d6ade734
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-10-06 00:07:40 +0100

    add

--- sale_bulk_cancel/wizards/bulk_cancel.py
+++ sale_bulk_cancel/wizards/bulk_cancel.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
 
 
-class CancelOrders(models.TransientModel):
+class CancelOrders(models.Model):
      _name = 'cancel.orders'
      _description = 'Cancel checked orders'
 
-     def sale_bulk_cancel(self):
+     def cancel(self):
           print("hello")
           active_ids = self._context.get('active_ids', []) or []
           for record in self.env['sale.order'].browse(active_ids):
