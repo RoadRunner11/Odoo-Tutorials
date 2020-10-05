8858b3fffdb470e7818202bc726f42b2f0c614a0
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-10-05 17:04:36 +0100

    add

--- sale_bulk_cancel/models/models.py
+++ sale_bulk_cancel/models/models.py
@@ -7,10 +7,10 @@ class CancelOrders(models.TransientModel):
     _name = 'cancel.orders'
     _description = 'Cancel checked orders'
 
-    def cancel(self):
+    def sale_bulk_cancel(self):
         active_ids = self._context.get('active_ids', []) or []
         for record in self.env['sale.order'].browse(active_ids):
-            record.state = "Cancelled"
+            record.state = "cancel"
 #
 #     @api.depends('value')
 #     def _value_pc(self):
