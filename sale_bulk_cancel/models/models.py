6d5ed8f85824b64177e91c4ce9773e4067d65f24
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-10-05 14:13:55 +0100

    add

--- sale_bulk_cancel/models/models.py
+++ sale_bulk_cancel/models/models.py
@@ -3,9 +3,9 @@
 from odoo import models, fields, api
 
 
-class sale_bulk_cancel(models.Model):
+class sale_bulk_cancel(models.TransientModel):
     _name = 'sale_bulk_cancel.sale_bulk_cancel'
-    _inherit = "sale.order"
+    _description = 'This allows a user to cancel all checked orders'
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
@@ -16,5 +16,7 @@ class sale_bulk_cancel(models.Model):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
     def action_cancel(self):
-        pass
+        active_ids = self._context.get("active_ids", []) or []
+        for record in self.env["sale.order"].browse(active_ids):
+            record.state = "Cancelled"
 
